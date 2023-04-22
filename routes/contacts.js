446a2be97b34dfe78f23763328be982f71018fba
Jonathaup <jonathanjacoburibepinto@gmail.com>
2023-04-22 00:13:11 -0600

    Working Test

---
+++ routes/contacts.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contacts');
+
+router.get('/', contactsController.getAll);
+router.get('/:id', contactsController.getSingle);
+
+
+
+// router.get('/:id', contactsController.getSingle);
+
+module.exports = router;
