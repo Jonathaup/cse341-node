3eb7e0088dda5ed2f298d4ed3ef3bfc95f22f61e
Jonathaup <jonathanjacoburibepinto@gmail.com>
2023-04-26 20:23:47 -0600

    Assignment 3

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
-app.use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+.use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
@@ -13,50 +20,3 @@ mongodb.initDb((err, mongodb) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
-
-
-
-
-
-
-
-// const express = require("express");
-// const connectDB = require('./db/connect');
-// const app = express();
-
-
-// connectDB();
-// app.use(express.json({extended:false}));
-// app.use("/api/userModel", require("./api/user"));
-// const port = process.env.PORT || 3000
-
-
-// // app.use("/", require("./routes"));
-
-
-
-// app.listen(3000, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
-
-
-
-
-
-
-// app.set("port",process.env.PORT || 3000);
-
-// app.get("/", getData);
-
-// function getData(req, res){
-//     console.log("getting data")
-//     res.write("{\"name\" : \"Shawn\"}");
-//     res.end();
-// }
-
-
-
-
-
-
-// app.listen(app.get("port"), () => console.log("server is listening on port" + app.get("port")));
